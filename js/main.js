3b0e30b1c194b94cda49f351343a5717c47c4c8a
austinnash80 <36436527+austinnash80@users.noreply.github.com>
2018-02-25 21:20:26 +1100

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,11 +1,11 @@
 var cards = [
 {	rank: "queen",
 	suit: "hearts",
-	cardImage: "images/gueen-of-hearts.png"
+	cardImage: "images/queen-of-hearts.png"
 },
 {	rank: "queen",
 	suit: "diamonds",
-	cardImage: "images/gueen-of-diamonds.png"
+	cardImage: "images/queen-of-diamonds.png"
 },
 {	rank: "king",
 	suit: "hearts",
@@ -13,26 +13,44 @@ var cards = [
 },
 {	rank: "king",
 	suit: "diamonds",
-	cardImage: "images/gueen-of-diamonds.png"
-},
+	cardImage: "images/king-of-diamonds.png"
+}
 ];
 var cardsInPlay = [];
 var checkForMatch = function(){
-}
 
-var flipCard = function(cardsId){
-checkForMatch();
-console.log("User flipped " + cards[cardsId].rank);
-cardsInPlay.push(cards[cardsId].rank);
-console.log(cards[cardsId].cardImage);
-console.log(cards[cardsId].suit);
-if (cardsInPlay.length === 2) {
-if (cardsInPlay [0] === cardsInPlay [1]) {
+
+	if (cardsInPlay [0] === cardsInPlay [1]) {
+	console.log("You Found a match!");	
 	alert("You found a match!");
 }
 else {
+	console.log("sorry, try again");
 	alert("sorry, try again");
+
 }}
+
+var flipCard = function(){
+var cardId = this.getAttribute('data-id');
+this.setAttribute('src', cards[cardId].cardImage);
+console.log("User flipped " + cards[cardId].rank);
+cardsInPlay.push(cards[cardId].rank);
+console.log(cards[cardId].cardImage);
+console.log(cards[cardId].suit);
+
+if (cardsInPlay.length === 2) { checkForMatch();
+
+}}
+
+
+var createBoard = function(){
+	for (var i = 0; i < cards.length; i++){
+	var cardElement = document.createElement('img');
+	cardElement.setAttribute('src','images/back.png');
+	cardElement.setAttribute('data-id', i);
+	cardElement.addEventListener('click', flipCard);
+	document.getElementById('game-board').appendChild(cardElement);
+	}
 }
-flipCard(0);
-flipCard(2);
+
+createBoard();
