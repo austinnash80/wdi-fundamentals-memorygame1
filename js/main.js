d2adea94e332019e848853b9f76d4a87f06098e4
austinnash80 <36436527+austinnash80@users.noreply.github.com>
2018-02-25 21:17:19 +1100

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,11 +1,17 @@
 var cards = ["queen", "queen", "king", "king"];
 var cardsInPlay = [];
-var cardOne = cards [0];
-cardsInPlay.push(cardOne);
-console.log("User flipped " + cardOne);
-var cardTwo = cards [2];
-cardsInPlay.push(cardTwo);
-console.log("User flipped " + cardTwo);
+
+var checkForMatch = function(){
+if (cardsInPlay[0] === cardsInPlay[1]) {
+console.log("You found a match!");
+} else {
+console.log("Sorry, try again.");
+}}
+
+var flipCard = function(cardId){
+checkForMatch();
+console.log("User flipped " + cards[cardId]);
+cardsInPlay.push(cards[cardId]);
 if (cardsInPlay.length === 2) {
 if (cardsInPlay [0] === cardsInPlay [1]) {
 	alert("You found a match!");
@@ -13,4 +19,6 @@ if (cardsInPlay [0] === cardsInPlay [1]) {
 else {
 	alert("sorry, try again");
 }}
-
+}
+flipCard(0);
+flipCard(2);
